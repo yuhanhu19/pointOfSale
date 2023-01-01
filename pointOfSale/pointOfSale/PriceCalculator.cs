ff8b006dc1803edbd591b5b08a442aed3c7d7eff
Yuhan Hu <yuhan.hu@cat.com>
2023-01-01 12:17:30 +0800

    [Yuhan] feat: implement total method to return sum of multiple item price

--- pointOfSale/pointOfSale/PriceCalculator.cs
+++ pointOfSale/pointOfSale/PriceCalculator.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace pointOfSale
 {
@@ -10,19 +13,22 @@ namespace pointOfSale
             {
                 throw new Exception("Error: empty barcode");
             }
-            if (barcode.Equals("12345"))
-            {
-                return "$7.25";
-            }
 
-            if (barcode.Equals("23456"))
+            switch (barcode)
             {
-                return "$12.50";
+                case "12345":
+                    return "$7.25";
+                case "23456":
+                    return "$12.50";
+                default:
+                    throw new Exception("Error: barcode not found");
             }
-            
-            throw new Exception("Error: barcode not found");
         }
-        
-        
+
+        public float Total(string barcodes)
+        {
+            var priceList = barcodes.Split(',').Select(barcode => GetPrice(barcode.Trim())).ToList();
+            return priceList.Select(price => float.Parse(price.TrimStart('$'))).ToList().Sum();
+        }
     }
 }
